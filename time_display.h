81e521256313abbd7978b7baba8991bc4fa671c3
Luis Lopes <web@luislopes.eu>
2025-03-20 14:34:06 +0000

    .

---
+++ time_display.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "esphome.h"
+
+void display_time(esphome::display::Display &display, float x, float y, 
+                 esphome::display::TextAlign align, const char* format, 
+                 esphome::Color color, const char* font) {
+  auto time = id(homeassistant_time).now();
+  if (!time.is_valid()) {
+    return;
+  }
+  
+  char buffer[64];
+  time.strftime(buffer, sizeof(buffer), format);
+  
+  display.printf(x, y, font, color, align, "%s", buffer);
+}
